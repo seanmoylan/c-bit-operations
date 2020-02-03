6b3ce33d2d3db5a94e7e6a83ee1b08edf4cd1004
Sean Moylan <sean.cheezy.moylan@gmail.com>
2020-02-03 22:41:08 +0000

    Added bit shifting operations

--- lookatbits.c
+++ lookatbits.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
 
-void printinbits(){
-  
+void printbits(uint8_t s){
+ 
+ for(int i = (8 - 1); i >= 0; i--){
+    printf("%d", ((1ULL << i) & s) ? 1 : 0);
+  }
 }
 
 
 int main(int argc, char *argv[]){
-	int i = 65;
-  char c = 65;
-  long int t = 65;
-  long long int m = 65;
-  
-  uint8_t u = 65;
-  uint64_t s = 65;
+  uint8_t a = 73;
+  uint8_t b = 99;
+  uint8_t c = (a & b);
 
-  printf("%x\t%c\t%d\t%d\n", c, c, c, sizeof(c));
-  printf("%x\t%c\t%d\t%d\n", i, i, i, sizeof(i));
-  printf("%x\t%c\t%d\t%d\n", t, t, t, sizeof(t));
-  printf("%x\t%c\t%d\t%d\n", m, m, m, sizeof(m));
+  printf("a = "); printbits(a); printf("\n");
+  printf("b = "); printbits(b); printf("\n");
+  printf("c = "); printbits(c); printf("\n");
+ 
+  printf("\n\n");
+
+  printf(" a = "); printbits(a); printf("\n");
+  printf("~a = "); printbits(~a); printf("\n");
+
+  printf("\n\n");
+
+  printf(" 1       = "); printbits( 1     ); printf("\n");
+  printf("(1 << 3) = "); printbits((1 << 3)); printf("\n");
+ 
+  printf("\n\n");
+
+  printf(" 99       = "); printbits(99); printf("\n");
+  printf("(99 << 3) = "); printbits((((uint8_t) 99) << ((uint8_t) 3))); printf("\n");
+ 
+  printf("\n\n");
+
+  printf("              99 = "); printbits(              99); printf("\n");
+  printf("(1ULL << 5)      = "); printbits((1ULL << 5)     ); printf("\n");
+  printf("(1ULL << 5) & 99 = "); printbits((1ULL << 5) & 99); printf("\n");
 
   return 0;
 }
